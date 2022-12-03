ee3bd53026d1ac57faa90a5d3a522ea23b6e631e
MrRoboto69 <a47529@alunos.ipb.pt>
2022-12-03 01:28:11 +0000

    Plans that Require Multiple Agents

---
+++ Assets/UpdateWorld.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateWorld : MonoBehaviour
+{
+    public Text states;
+
+    void LateUpdate()
+    {
+        Dictionary<string, int> WorldStates = GWorld.Instance.GetWorld().GetStates();
+        states.text = "";
+        foreach (KeyValuePair<string, int> s in WorldStates)
+        {
+            states.text += s.Key + ", " + s.Value + "\n";
+        }
+    }
+}
