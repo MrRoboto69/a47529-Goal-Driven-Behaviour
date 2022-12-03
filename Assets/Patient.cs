2903395dffdb3f4c34303bd6b2581159bb27f64d
MrRoboto69 <a47529@alunos.ipb.pt>
2022-12-03 00:58:51 +0000

    Creating a Multi-step Plan

--- Assets/Patient.cs
+++ Assets/Patient.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Patient : GAgent
 {
     // Start is called before the first frame update
-    void Start()
+    new void Start()
     {
         base.Start();
         SubGoal s1 = new SubGoal("isWaiting", 1, true);
-        goals.Add(s1, 3);
+        goals.Add(s1, 3); 
     }
 
 }
